728c8d84177b625da459d38ba55c0e1f50eeeb3d
Tomasz Kaczmarek <tkaczmarek2001@gmail.com>
2024-03-19 18:07:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/culture_reminder/Register.kt
@@ -0,0 +1,13 @@
+package com.example.culture_reminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
